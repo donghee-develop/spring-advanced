de1a4c445b572de1ba4fb4e6fead0316e5e1e266
aksirjwn <skks1000@naver.com>
2025-04-21 14:53:38 +0900

    feat : timestamp 상속

--- src/main/java/org/example/expert/domain/manager/entity/Manager.java
+++ src/main/java/org/example/expert/domain/manager/entity/Manager.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.manager.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.entity.User;
 
@@ -10,7 +11,7 @@ import org.example.expert.domain.user.entity.User;
 @Entity
 @NoArgsConstructor
 @Table(name = "managers")
-public class Manager {
+public class Manager extends Timestamped {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
