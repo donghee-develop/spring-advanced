0d6fad5ad1a8e536a7d1743e598ef2dd37e19ada
aksirjwn <skks1000@naver.com>
2025-04-21 17:24:33 +0900

    fix : 오타

--- src/main/java/org/example/expert/config/ErrorCode.java
+++ src/main/java/org/example/expert/config/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     COMMENT_NOT_FOUND("존재하지 않는 댓글입니다", HttpStatus.NOT_FOUND),
     TODO_NOT_FOUND("존재하지 않는 글입니다.", HttpStatus.NOT_FOUND),
     USER_NOT_FOUND("존재하지 않는 유저입니다.", HttpStatus.NOT_FOUND),
-    MANAGER_NOT_FOUND("존재하[지 않는 매니저입니다.",HttpStatus.NOT_FOUND),
+    MANAGER_NOT_FOUND("존재하지 않는 매니저입니다.",HttpStatus.NOT_FOUND),
 
     CANNOT_REGISTER_SELF_AS_MANAGER("일정 작성자는 본인을 담당자로 등록할 수 없습니다.", HttpStatus.BAD_REQUEST),
     SAME_AS_OLD_PASSWORD("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.", HttpStatus.BAD_REQUEST),
